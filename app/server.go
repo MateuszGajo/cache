3dc8229d16cb4528bac3f63df0b8762b5562944e
Mateusz <mateusz.gajo9@wp.pl>
2024-05-05 12:08:43 +0200

    test

--- app/server.go
+++ app/server.go
@@ -77,7 +77,7 @@ type Server struct {
 // }
 
 
-func handShake(){
+func handShake() error{
 	fmt.Print("replica handshake?")
 	fmt.Println("tcp", replica.Address + ":" + replica.Port)
 	conn, err := net.Dial("tcp", replica.Address + ":" + replica.Port)
@@ -133,7 +133,9 @@ func handShake(){
 	// 	os.Exit(1)
 	// }
 
-	// defer conn.Close()
+	defer conn.Close()
+
+	return nil;
 
 	// go handleConenction(conn, Server{})
 }
