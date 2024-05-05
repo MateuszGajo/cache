d064e8b5aaec50c153f92f04c9f846afac7babaa
Mateusz <mateusz.gajo9@wp.pl>
2024-05-05 13:08:11 +0200

    run tesT

--- app/server.go
+++ app/server.go
@@ -116,13 +116,13 @@ func handShake(){
 
 	conn.Write([]byte("*3"+CLRF+"$5"+CLRF+"PSYNC"+CLRF+"$1"+CLRF+"?"+CLRF+"$2"+CLRF+"-1"+CLRF))
 	fmt.Println("sending psync")
-	// inputComm = readInput(conn)
-	// args = inputComm.commandStr
+	inputComm = readInput(conn)
+	args = inputComm.commandStr
 
-	// fmt.Print("response")
-	// fmt.Println(inputComm.commandStr)
-	// inputComm = readInput(conn)
-	// fmt.Print("i hope we are not here")
+	fmt.Print("response")
+	fmt.Println(inputComm.commandStr)
+	inputComm = readInput(conn)
+	fmt.Print("i hope we are not here")
 
 
 
